d0370478723db4b21233fb5caf3c147a125e6a15
James Kenneth <takashimaretubado@gmail.com>
2025-04-01 22:14:03 +0800

    initial icon and splash screen

--- README.md
+++ README.md
@@ -48,3 +48,6 @@ Join our community of developers creating universal apps.
 
 - [Expo on GitHub](https://github.com/expo/expo): View our open source platform and contribute.
 - [Discord community](https://chat.expo.dev): Chat with Expo users and ask questions.
+
+
+- https://www.flaticon.com/free-icons/checkers" Checkers icons created by Freepik - Flaticon
---
+++ app/index.tsx
@@ -0,0 +1,51 @@
+import { View, Text, StyleSheet, ImageBackground } from 'react-native'
+import React from 'react'
+import { Link } from 'expo-router'
+
+const app = () => {
+  return (
+    <View style = {styles.container}>
+      <ImageBackground
+        source={require('@/assets/images/checkers.png')}
+        resizeMode='cover'
+        style={styles.image}
+        >
+      <Text style = {styles.title}>Checkers</Text>
+      <Link href="/explore" style = {styles.link}>Explore</Link>
+      </ImageBackground>
+    </View>
+  )
+}
+
+export default app
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    flexDirection: 'column',
+  },
+  image:{
+    width: '100%',
+    height: '100%',
+    flex: 1,
+    resizeMode: 'cover',
+    justifyContent: 'center',
+  },
+  title:{
+    color: 'white',
+    fontSize: 42,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    backgroundColor: 'rgba(0,0,0,0.5)',
+    marginBottom: 120,
+  },
+  link:{
+    color: 'white',
+    fontSize: 42,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    textDecorationLine: 'underline',
+    backgroundColor: 'rgba(0,0,0,0.5)',
+    padding: 4,
+  },
+})
